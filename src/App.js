7e813f46f9de37b76c8dd6049ce4976e23445d99
Jacob <jacobc1596@gmail.com>
2020-09-21 16:44:39 +0100

    Added dark-mode feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Grid from './components/Grid';
 import Footer from './components/Footer';
 
 function App() {
+
+  
   return (
     <div className="App">
       <Header/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ const Header = ()=>{
     const [toggle, setToggle] = useState(false);
 
     function handleChange(event){
-        setToggle(prevToggle => !prevToggle);
-        console.log(toggle)
+        setToggle(!toggle);
     }
+    
+    toggle ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
 
     return(
+
         <header className="nav-container">
 
                 <label className="switch">
